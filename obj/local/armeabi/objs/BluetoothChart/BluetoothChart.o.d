9d117201dfd465a9dadd8a4d4ba4dbc417ae82ee
kylinxiang <337403807@qq.com>
2015-07-01 20:06:45 +0800

    add project

---
+++ obj/local/armeabi/objs/BluetoothChart/BluetoothChart.o.d
@@ -0,0 +1,20 @@
+obj/local/armeabi/objs/BluetoothChart/BluetoothChart.o: \
+ jni/BluetoothChart.cpp \
+ F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/string.h \
+ F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/malloc.h \
+ F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/jni.h
+
+F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/string.h:
+
+F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+F:/work/android_env/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/jni.h:
